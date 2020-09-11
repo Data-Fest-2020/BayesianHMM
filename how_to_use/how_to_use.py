906424f9bd7eff9f2f857dd1d7f6b2e93a0aa84e
matthiasgruber <52892364+matthiasgruber@users.noreply.github.com>
2020-09-11 15:47:47 +0200

    update

--- how_to_use/how_to_use.py
+++ how_to_use/how_to_use.py
@@ -1,93 +1,74 @@
 """
-Created on Sat Aug 29 11:13:10 2020
+@author: matthias.gruber@outlook.com
 
-@author: sds
+how_to_use.py
 """
 
 # import modules
 import hiddensmmodel
-import load_data
+import data_manipulation
 from matplotlib import pyplot as plt
 import pandas as pd
 
-# load data: nparray without timestamp
 mytool = "electric_screwdriver"
 #mytool = "pneumatic_screwdriver"
 #mytool = "pneumatic_rivet_gun"
 
-data = load_data.load_data(mytool)
+# load data (and smooth it)
+data = data_manipulation.load_data(mytool = mytool, smooth = False,
+                                   windowSize = 40)
 
 # train model
-model = hiddensmmodel.Hsmm(data[13], Nmax = 8, trunc = 1000)
+model = hiddensmmodel.Hsmm(data[13][0:70000], Nmax = 15, trunc = 200)
+# get the true labels of the training set
+y_true_train = data[14][0:70000]
+# get the predicted and true labels of the test set
+y_hat_test_un = model.predict(data[13][70001:])
+y_true_test = data[14][70001:]
+# match the predicted labels to the true ones and smooth outliers out
+y_hat_test = data_manipulation.map_states(model, y_hat_test_un, y_true_train,
+                                          smooth = True, windowSize = 19)
 
-y_hat = model.states
-y_hat = y_hat[0]
-y_hat = pd.DataFrame(y_hat)
-y_true = pd.DataFrame(data[14])
-y_true = y_true.rename(columns={0: 'y_true'})
-y_hat = y_hat.rename(columns={0: 'y_hat'})
-y_hat = y_hat + 1500
-
-y = pd.DataFrame()
-y = pd.concat([y_true, y_hat], axis=1)
-
-y['count'] = 1
-
-y_manC = y.groupby(['y_true','y_hat']).count()
-
-y_manC = y_manC.reset_index()
-
-states_hat = y_manC['y_hat'].unique()
-
-for state_hat in states_hat:
-    idx = y_manC['y_hat'][y_manC['y_hat']==state_hat]
-    idx = idx.reset_index()
-    help1 = y_manC.iloc[idx['index'],:]
-    maxS = help1.iloc[help1['count'].argmax()]
-    y_hat = y_hat.replace({maxS['y_hat']:maxS['y_true']})    
+y_true_test = pd.DataFrame(y_true_test)
+y_hat_test = pd.DataFrame(y_hat_test)
+y_hat_test = y_hat_test.rename(columns={0: 'y_hat'})
+y_true_test = y_true_test.rename(columns={0: 'y_true'})
+y_pneumatic_screwdriver = pd.concat([y_true_test, y_hat_test], axis=1)
+y_pneumatic_screwdriver.to_csv('y_pneumatic_screwdriver.csv')
 
 
-
-
-plt.plot(y_true[10000:15000], 'bo', markersize=0.5)
+plt.plot(y_true_test[1:5000], 'bo', markersize=0.5)
 plt.show()
 
-plt.plot(y_hat[10000:15000], 'bo', markersize=0.5)
+plt.plot(y_hat_test[1:5000], 'bo', markersize=0.5)
 plt.show()
 
-plt.plot(y_true[1:5000], 'bo', markersize=0.5)
+plt.plot(y_hat_test_un[1:5000], 'bo', markersize=0.5)
 plt.show()
 
-plt.plot(y_hat[1:5000], 'bo', markersize=0.5)
+plt.plot(y_true_test[10000:15000], 'bo', markersize=0.5)
 plt.show()
 
-plt.plot(y_true[15000:20000], 'bo', markersize=0.5)
+plt.plot(y_hat_test[10000:15000], 'bo', markersize=0.5)
 plt.show()
 
-plt.plot(y_hat[15000:20000], 'bo', markersize=0.5)
+plt.plot(y_true_test[15000:20000], 'bo', markersize=0.5)
 plt.show()
 
-plt.plot(y_true[40000:45000], 'bo', markersize=0.5)
+plt.plot(y_hat_test[15000:20000], 'bo', markersize=0.5)
 plt.show()
 
-plt.plot(y_hat[40000:45000], 'bo', markersize=0.5)
+plt.plot(y_true_test[20000:25000], 'bo', markersize=0.5)
 plt.show()
 
-plt.plot(y_true[65000:70000], 'bo', markersize=0.5)
+plt.plot(y_hat_test[20000:25000], 'bo', markersize=0.5)
 plt.show()
 
-plt.plot(y_hat[65000:70000], 'bo', markersize=0.5)
+plt.plot(y_true_test, 'bo', markersize=0.5)
 plt.show()
 
-plt.plot(y_true[135000:140000], 'bo', markersize=0.5)
+plt.plot(y_hat_test, 'bo', markersize=0.5)
 plt.show()
 
-plt.plot(y_hat[135000:140000], 'bo', markersize=0.5)
-plt.show()
 
-plt.plot(y_true, 'bo', markersize=0.5)
-plt.show()
-
-plt.plot(y_hat, 'bo', markersize=0.5)
-plt.show()
 
